a4bb8de7be62785c13dd6b4bb07d3d295c116756
Shailesh Godghase <shaileshgodghase19@gmail.com>
2022-10-31 19:19:43 +0530

    Added Loading Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom';
 import Home from './Components/Home';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Feature from './Components/Feature';
-import Meeting from './Components/Meeting';
 import NotFound from './Components/NotFound';
+import Loading from './Components/Loading';
+const About = lazy(() => import('./Components/About'));
+const Contact = lazy(() => import('./Components/Contact'));
+const Feature = lazy(() => import('./Components/Feature'));
+const Meeting = lazy(() => import('./Components/Meeting'));
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="feature" element={<Feature />} />
-          <Route path="meeting/:meetingId" element={<Meeting />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="feature" element={<Feature />} />
+            <Route path="meeting/:meetingId" element={<Meeting />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </>
   );
 }
